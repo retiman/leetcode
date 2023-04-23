239ffd5414381cb434f7e7f987b6b8a10987a4eb
Min Huang <57320+retiman@users.noreply.github.com>
2023-04-22 18:53:49 -0700

    Updates the CI workflow (#3)
    Co-authored-by: github-actions[bot] <github-actions[bot]@github.com>

---
+++ test/two-sum.test.ts
@@ -0,0 +1,17 @@
+export function find(xs: number[], target: number) {
+  for (let i = 0; i < xs.length; i += 1) {
+    for (let j = i + 1; j < xs.length; j += 1) {
+      if (xs[i] + xs[j] === target) {
+        return [i, j];
+      }
+    }
+  }
+}
+
+describe('two sum', () => {
+  test('run', async () => {
+    expect(find([2, 7, 11, 15], 9)).toStrictEqual([0, 1]);
+    expect(find([3, 2, 4], 6)).toStrictEqual([1, 2]);
+    expect(find([3, 3], 6)).toStrictEqual([0, 1]);
+  });
+});
