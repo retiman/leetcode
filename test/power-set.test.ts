83c1972020d5f7bd6309b0bff5d10a653a4de215
Min Huang <57320+retiman@users.noreply.github.com>
2023-04-23 20:56:10 -0700

    Adds code for generating power sets (#6)

---
+++ test/power-set.test.ts
@@ -0,0 +1,42 @@
+describe('power set', () => {
+  function __ps(xs: Set<number>): Set<Set<number>> {
+    if (xs.size === 0) {
+      // Note that new Set(new Set()) gives you a set with zero elements, because the constructor takes all elements of
+      // the first argument and adds them to the set.  So you actually want to construct a new iterable based on the
+      // empty set.
+      return new Set([new Set()]);
+    }
+
+    // Find the first element of xs and call it x; remove it from the list.
+    const x = xs.values().next().value;
+
+    // Generate all subsets without x.
+    const rest = new Set(xs);
+    rest.delete(x);
+
+    const excluded: Set<Set<number>> = __ps(rest);
+
+    // Add x to all of the subsets from above.
+    const included: Set<Set<number>> = new Set();
+    excluded.forEach(ex => {
+      const rest = new Set(ex);
+      rest.add(x);
+
+      included.add(rest);
+    });
+
+    // The subsets with x and the subsets without x constitute the power set.
+    return new Set([...excluded, ...included]);
+  }
+
+  function ps(xs: Array<number>) {
+    return __ps(new Set(xs));
+  }
+
+  test('run', async () => {
+    expect(ps([])).toMatchSnapshot();
+    expect(ps([1])).toMatchSnapshot();
+    expect(ps([1, 2])).toMatchSnapshot();
+    expect(ps([1, 2, 3])).toMatchSnapshot();
+  });
+});
