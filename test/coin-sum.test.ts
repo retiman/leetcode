223b7c3992daa14ded93e4c2de9c6cc639ea72b7
Min Huang <min.huang@alumni.usc.edu>
2023-04-25 13:30:44 -0700

    nice

--- test/coin-sum.test.ts
+++ test/coin-sum.test.ts
@@ -1,16 +1,20 @@
 describe('coin sum', () => {
   function sum(coins: Array<number>, target: number) {
-    // Fills an array of X size with 0's.
-    const ways = new Array(target).fill(0);
+    // Fills an array of X+1 size with 0's; each index will represent a target.  We need an extra index because the
+    // index is the target, so we need to account for ways[200] is the number of ways to make 200, and ways[0] is the
+    // number of ways to make 0.  This is all because arrays are 0 indexed.
+    const ways = new Array(target + 1).fill(0);
 
-    ways.unshift(1);
+    // With a target of 0, there's only one way to do this: with no coins.
+    ways[0] = 1;
 
     coins.forEach(coin => {
-      for (let i = coin; i <= target; i += 1) {
-        ways[i] += ways[i - coin];
+      for (let j = coin; j <= target; j += 1) {
+        ways[j] += ways[j - coin];
       }
     });
 
+    console.log(ways);
     return ways[target];
   }
 
