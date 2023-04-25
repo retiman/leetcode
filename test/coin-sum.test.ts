56b85ba7da19df970a0a8e952df6d2298f9fac85
Min Huang <min.huang@alumni.usc.edu>
2023-04-25 13:37:12 -0700

    ok

--- test/coin-sum.test.ts
+++ test/coin-sum.test.ts
@@ -9,6 +9,23 @@ describe('coin sum', () => {
     ways[0] = 1;
 
     coins.forEach(coin => {
+      // After introducing a coin denomination, all ways to compute target "j" need to be updated with the number of
+      // ways we can make previous targets without that coin.
+      //
+      // For example, if we have 1 way to make 2 cents with a penny, then adding a 2-cent coin into circulation would
+      // mean that we have 1 way to make 2 cents with a penny PLUS 1 way to make 2 cents with a 2-cent coin.
+      //
+      // So initially, we would have:
+      //
+      // ways[0] = 1
+      // ways[1] = 1
+      // ways[2] = 1
+      //
+      // Introducing the 2-cent coin would mean that:
+      //
+      // ways[0] = 1
+      // ways[1] = 1
+      // ways[2] = 2
       for (let j = coin; j <= target; j += 1) {
         ways[j] += ways[j - coin];
       }
@@ -19,6 +36,7 @@ describe('coin sum', () => {
   }
 
   test('run', async () => {
-    expect(sum([1, 2, 5, 10, 20, 50, 100, 200], 200)).toBe(73682);
+    expect(sum([1, 2], 2)).toBe(2);
+    //expect(sum([1, 2, 5, 10, 20, 50, 100, 200], 200)).toBe(73682);
   });
 });
