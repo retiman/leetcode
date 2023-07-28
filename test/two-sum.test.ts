d0d7311f5779003e233e7109e055a425e334f4e5
Min Huang <min.huang@alumni.usc.edu>
2023-07-27 18:40:35 -0700

    adds instructions for two sum

--- test/two-sum.test.ts
+++ test/two-sum.test.ts
@@ -1,3 +1,11 @@
+// Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to
+// target.
+//
+// You may assume that each input would have exactly one solution, and you may not use the same element twice.
+//
+// You can return the answer in any order.
+//
+// See https://leetcode.com/problems/two-sum
 describe('two sum', () => {
   function find(xs: number[], target: number) {
     for (let i = 0; i < xs.length; i += 1) {
