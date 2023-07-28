8d35c047eded4d1d629a09a50f27668b1b44538b
Min Huang <57320+retiman@users.noreply.github.com>
2023-07-27 18:39:10 -0700

    Adds a faster version of two sum (#13)

--- test/two-sum.test.ts
+++ test/two-sum.test.ts
@@ -9,9 +9,38 @@ describe('two sum', () => {
     }
   }
 
+  function findFaster(xs: number[], target: number) {
+    // Do the same as before but use a hashmap to keep track of values we've already seen.
+    const m = new Map<number, number>();
+    for (let i = 0; i < xs.length; i += 1) {
+      const x = xs[i];
+      const y = target - x;
+      // If our map has the other value that would make up the target, we can return it immediately.
+      if (m.has(y)) {
+        return [i, m.get(y)];
+      }
+
+      // Otherwise, loop through the list as usual.
+      for (let j = i + 1; j < xs.length; j += 1) {
+        const y = xs[j];
+        m.set(y, j);
+
+        if (x + y === target) {
+          return [i, j];
+        }
+      }
+    }
+  }
+
   test('run', async () => {
     expect(find([2, 7, 11, 15], 9)).toStrictEqual([0, 1]);
     expect(find([3, 2, 4], 6)).toStrictEqual([1, 2]);
     expect(find([3, 3], 6)).toStrictEqual([0, 1]);
+  });
+
+  test('run faster', async () => {
+    expect(findFaster([2, 7, 11, 15], 9)).toStrictEqual([0, 1]);
+    expect(findFaster([3, 2, 4], 6)).toStrictEqual([1, 2]);
+    expect(findFaster([3, 3], 6)).toStrictEqual([0, 1]);
   });
 });
