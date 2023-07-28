2aa62249ff0f180dc23fb6062ef6063cb6f9317e
Min Huang <57320+retiman@users.noreply.github.com>
2023-07-27 19:02:30 -0700

    Adds descriptions for the leetcode problems (#14)

--- test/coin-sum.test.ts
+++ test/coin-sum.test.ts
@@ -1,3 +1,13 @@
+// In the United Kingdom the currency is made up of pound (£) and pence (p). There are eight coins in general
+// circulation:
+//
+// 1p, 2p, 5p, 10p, 20p, 50p, £1 (100p), and £2 (200p).
+// It is possible to make £2 in the following way:
+//
+// 1×£1 + 1×50p + 2×20p + 1×5p + 1×2p + 3×1p
+// How many different ways can £2 be made using any number of coins?
+//
+// See https://projecteuler.net/problem=31
 describe('coin sum', () => {
   function sum(coins: number[], target: number) {
     // Fills an array of X+1 size with 0's; each index will represent a target.  We need an extra index because the
--- test/dfs.test.ts
+++ test/dfs.test.ts
@@ -1,3 +1,10 @@
+// Depth first search can be used to solve a variety of problems.
+//
+// For a DAG, a reverse postorder traversal gives you a topological sort.
+//
+// The binary tree inorder traversal is done with DFS.
+//
+// See https://leetcode.com/problems/binary-tree-inorder-traversal/
 describe('depth first search', () => {
   type Vertex = string;
   type Edge = [string, string];
@@ -25,6 +32,8 @@ describe('depth first search', () => {
       return [this.preorder, this.postorder];
     }
 
+    // The general strategy is to preorder visit the node, then recursively call this function on frontier nodes.
+    // Finally postorder visit the node.
     private runInternal(u: string) {
       if (this.visited.has(u)) {
         return;
