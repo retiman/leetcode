54aa5e9d6634fa5facc67f98a50622f5e093dd12
Min Huang <min.huang@alumni.usc.edu>
2024-03-25 13:37:24 -0700

    adds solution for valid parens

--- test/leetcode/valid-parentheses.test.ts
+++ test/leetcode/valid-parentheses.test.ts
@@ -8,6 +8,40 @@
 //
 // See https://leetcode.com/problems/valid-parentheses/
 describe('valid parentheses', () => {
+  function isValid(text: string) {
+    const map = new Map<string, string>();
+    map.set('(', ')');
+    map.set('{', '}');
+    map.set('[', ']');
+
+    const stack = new Array<string>();
+    for (let i = 0; i < text.length; i++) {
+      const c = text[i];
+
+      if (c === '(' || c === '{' || c === '[') {
+        stack.push(c);
+        continue;
+      }
+
+      if (c === ')' || c === '}' || c === ']') {
+        if (stack.length === 0) {
+          return false;
+        }
+
+        const open = stack.pop();
+        const close = map.get(open || '');
+        if (close !== c) {
+          return false;
+        }
+      }
+    }
+
+    return true;
+  }
+
   test('run', async () => {
+    expect(isValid('()')).toBe(true);
+    expect(isValid('()[]{}')).toBe(true);
+    expect(isValid('(}')).toBe(false);
   });
 });
