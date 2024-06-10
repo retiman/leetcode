65a12bf21ec0ca8d0d714d0189bf93b5a0fb3d10
Min Huang <min.huang@alumni.usc.edu>
2024-06-10 16:06:13 -0700

    moved

--- test/leetcode/recursion/generate-parentheses.test.ts
+++ test/leetcode/recursion/generate-parentheses.test.ts
@@ -2,7 +2,7 @@
 //
 // Given n pairs of parentheses, write a function to generate all combinations of well-formed parentheses.
 //
-// See https://leetcode.com/problems/generate-parentheses/
+// See https://leetcode.com/problems/generate-parentheses
 describe('generate parentheses', () => {
   // A naive way to do this would be to do something like generate all the parentheses for n-1, then add more
   // parentheses to each of the elements generated.  For example, by transforming each element of the n-1 solution to
