78f4204ea6113711e9cee848d9726b348d4312de
Min Huang <min.huang@alumni.usc.edu>
2024-06-25 22:36:44 -0700

    ok

--- test/two-pointer/container-with-most-water.test.ts
+++ test/two-pointer/container-with-most-water.test.ts
@@ -11,13 +11,17 @@
 //
 // See https://leetcode.com/problems/container-with-most-water/
 describe('container with most water', () => {
+  // Use the two pointers technique to maximize the area.  We'll initialize at the left and right sides of the array,
+  // then advance the pointers inward and update our knowledge of the maximum area.
+  //
+  // We'll want to go from outside to inside, because if we have the highest columns on the outside, it's trivial to
+  // calculate the max area container.  The problem is if we have shorter columns over a long width, but we can adjust
+  // the pointers and max area as we move them inwards.
   function maxArea(xs: number[]): number {
     let max = 0;
     let left = 0;
     let right = xs.length - 1;
 
-    // Use the two pointers technique to maximize the area.  We'll initialize at the left and right sides of the array,
-    // then advance the pointers inward and update our knowledge of the maximum area.
     while (left < right) {
       // Set the width to be the difference between left and right pointers.  The height should be the smaller of the
       // two heights referenced by the pointers.
@@ -27,10 +31,14 @@ describe('container with most water', () => {
       // Update the max area.
       max = Math.max(max, width * height);
 
-      // Now advance the pointers.
+      // Now advance the pointers.  No matter which pointer we choose to advance, the width will decrease.
       //
-      // No matter which pointer we choose to advance, the width will decrease.  Try to compensate for the resulting
-      // difference by advancing the pointer that points to the smaller height first.
+      // To attempt to maximize the area, we should advance the pointer that points to the lowest height, in some hopes
+      // that we will reach a higher height area and get a bigger container.
+      //
+      // Note that if we advance a pointer and the height ends up lower, we'll end up with an area calculation that does
+      // not correspond to an actual container.  However, that "phantom container" will have a smaller max area than
+      // what was previously calculated, and it won't affect the actual result.
       if (xs[left] < xs[right]) {
         left++;
       } else {
