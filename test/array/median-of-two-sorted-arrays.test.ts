e687453d54b9ede993ee53c0fd4db5b239ef9bc5
Min Huang <min.huang@alumni.usc.edu>
2024-07-07 19:19:08 -0700

    m

--- test/array/median-of-two-sorted-arrays.test.ts
+++ test/array/median-of-two-sorted-arrays.test.ts
@@ -101,8 +101,8 @@ describe('median of two sorted arrays', () => {
       //
       // Using (m+n+1)/2 does not affect the calculation for an even sized "merged" array because the floor function
       // removes the decimal.
-      const p1 = Math.floor((m + n) / 2);
-      const p2 = (m + n) % 2 === 0 ? (m + n) / 2 - p1 : Math.floor((m + n + 1) / 2) - p1;
+      const p1 = Math.floor((left + right) / 2); // Note that p1 needs to be calculated using left/right.
+      const p2 = Math.floor((m + n + 1) / 2) - p1; // Note that p2 needs to be calculated based on total size.
 
       // Now we imagine that the two left partitions are merged, and the two right partitions are merged.  If we've
       // chosen the partition points correctly, then the last value of both left partitions should be less than the
@@ -111,25 +111,41 @@ describe('median of two sorted arrays', () => {
       // Here's an example partition of two arrays, where p1=2 and p2=3.
       //
       // xs = [1,2   | 3,3]   with lmax1=2 and rmin1=3 (lmax1 is at p1-1 due to zero indexing).
-      // ys = [1,2,3 | 4,5,5] with lmax2=3 and rmin2=4 (lmax2 is at p2-2 due to zero indexing).
+      // ys = [1,2,3 | 4,5,5] with lmax2=3 and rmin2=4 (lmax2 is at p2-1 due to zero indexing).
       //
       // In this case, we want to compare the rightmost values of xs with the opposite leftmost value of ys, and vice
       // versa.
-      const lmax1 = xs[p1 - 1];
-      const lmax2 = ys[p2 - 1];
-      const rmin1 = xs[p1];
-      const rmin2 = ys[p2];
+      //
+      // Note that if p1 or p2 are zero, that means that the partitioning leaves no elements in the partition, so
+      // those min/max values should be replaced by -Infinity or +Infinity.
+      //
+      // Consider the following cases:
+      //
+      // xs = []       with lmax1=-Infinity because there is no value, and everything is >lmax1.
+      // xs = [1,2,3|] with rmax1=+Infinity because there is no value, and everything is <rmin1.
+      const lmax1 = p1 === 0 ? -Infinity : xs[p1 - 1]; // If xs partition is empty, lmax1 value is unused.
+      const rmin1 = p1 === m ? Infinity : xs[p1]; // If xs partition is full, rmin1 value is unused.
+      const lmax2 = p2 === 0 ? -Infinity : ys[p2 - 1]; // If ys partition is empty, lmax2 value is unused.
+      const rmin2 = p2 === n ? Infinity : ys[p2]; // If ys partition is full, rmin2 value is unused.
 
       // If our inequalities hold, we have found the median.  The value differs depending on if we have an odd sized
       // "merged" array or even.
       if (lmax1 <= rmin2 && lmax2 <= rmin1) {
-        return m + (n % 2) === 0 ? (Math.max(lmax1, lmax2) + Math.min(rmin1, rmin2)) / 2 : Math.max(lmax1, lmax2);
+        // Even case
+        if ((m + n) % 2 === 0) {
+          return (Math.max(lmax1, lmax2) + Math.min(rmin1, rmin2)) / 2;
+        }
+
+        // Odd case.
+        return Math.max(lmax1, lmax2);
       }
+
       // Here the xs side left partition's last value is too large, so we have to update the right pointer.  At the next
       // iteration we will do a binary search where the right pointer has been set to the element before p1.
       if (lmax1 > rmin2) {
         right = p1 - 1;
       }
+
       // Here the xs side left partition's last value is too small, so we have to change the left pointer.  At the next
       // iteration we will do binary search where the left pointer has been set to the element after p1.
       else {
@@ -150,5 +166,13 @@ describe('median of two sorted arrays', () => {
 
   test('naive test case 3', async () => {
     expect(__naiveFindMedianSortedArrays([], [1])).toBe(1);
+  });
+
+  test('test case 1', async () => {
+    expect(findMedianSortedArrays([1, 3], [2])).toBe(2);
+  });
+
+  test('test case 2', async () => {
+    expect(findMedianSortedArrays([3, 2, 3, 1, 2, 4, 5, 5, 6], [4])).toBe(3);
   });
 });
