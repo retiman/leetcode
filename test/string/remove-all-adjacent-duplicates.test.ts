d79f4a16a15b6259d7700472651b38d832cfce30
Min Huang <57320+retiman@users.noreply.github.com>
2024-07-29 18:57:48 -0700

    SOLN: Remove all adjacent duplicates (#105)
    Co-authored-by: github-actions[bot] <github-actions[bot]@github.com>

---
+++ test/string/remove-all-adjacent-duplicates.test.ts
@@ -0,0 +1,33 @@
+// DIFFICULTY: Easy
+//
+// You are given a string s consisting of lowercase English letters. A duplicate removal consists of choosing two
+// adjacent and equal letters and removing them.
+//
+// We repeatedly make duplicate removals on s until we no longer can.
+//
+// Return the final string after all such duplicate removals have been made. It can be proven that the answer is unique.
+//
+// See https://leetcode.com/problems/remove-all-adjacent-duplicates-in-string/
+describe('remove all adjacent duplicates', () => {
+  function removeDuplicates(s: string) {
+    const stack: string[] = [];
+
+    for (const c of s) {
+      // If the top of the stack has a dupe, pop it off and ignore this character.
+      if (stack.length !== 0 && c === stack[stack.length - 1]) {
+        stack.pop();
+        continue;
+      }
+
+      // Otherwise push the character on the stack.
+      stack.push(c);
+    }
+
+    // We'll end up with all the non-dupes.
+    return stack.join('');
+  }
+
+  test('remove all adjacent duplicates - test case 1', async () => {
+    expect(removeDuplicates('abbaca')).toBe('ca');
+  });
+});
