1d555b7eeb84ca15ee7c0a6d2f3d5f8d828b18e3
Min Huang <min.huang@alumni.usc.edu>
2024-12-10 21:57:32 -0800

    skip these

--- src/heap/bs-search-heap.ts
+++ src/heap/bs-search-heap.ts
@@ -4,8 +4,9 @@ export interface Element<T> {
 }
 
 // When there isn't time to implement a real heap in an interview, and the @datastructures-js/priority-queue library
-// isn't available, just insert and sort.  This will be O(nlogn) on insert but it's quick and dirty.
-export class SimpleMaxHeap<T> {
+// isn't available, you can use binary search insert in place of a real heap.  This will be O(1) for popping, but it
+// will be O(n) for insert (even though the binary search is O(logn), we do have to resize the array at O(n)).
+export class BinarySearchMaxHeap<T> {
   private readonly heap: Element<T>[];
 
   private readonly compare: (a: number, b: number) => number;
@@ -20,8 +21,22 @@ export class SimpleMaxHeap<T> {
       value,
       priority: this.getPriority(value, priority)
     };
-    this.heap.push(element);
-    this.heap.sort((a, b) => this.compare(a.priority, b.priority));
+
+    let left = 0;
+    let right = this.heap.length;
+    while (left < right) {
+      const mid = Math.floor((left + right) / 2);
+      const a = this.heap[mid];
+      const b = element;
+      // You can use < here for non-stable inserts, if you don't care.  Switch to <= for stable inserts.
+      if (this.compare(a.priority, b.priority) < 0) {
+        left = mid + 1;
+      } else {
+        right = mid;
+      }
+    }
+
+    this.heap.splice(left, 0, element);
   }
 
   public dequeue(): Element<T> {
--- test/heap/kth-largest-element.test.ts
+++ test/heap/kth-largest-element.test.ts
@@ -10,6 +10,8 @@
 import fs from 'fs';
 import path from 'path';
 import { MaxPriorityQueue } from '@datastructures-js/priority-queue';
+import { SimpleMaxHeap } from '../../src/heap/simple-heap';
+import { BinarySearchMaxHeap } from '../../src/heap/bs-search-heap';
 
 describe('kth largest element in an array', () => {
   // You can use quick select to do the most efficient algorithm, but it's very complicated.  Instead, we'll just use
@@ -32,12 +34,45 @@ describe('kth largest element in an array', () => {
     return result;
   }
 
+  // You can use a simple heap that sorts in a pinch.
+  function findKthLargestSimple(nums: number[], k: number): number {
+    const heap = new SimpleMaxHeap<number>();
+    for (const num of nums) {
+      heap.enqueue(num);
+    }
+
+    for (let i = 0; i < k - 1; i++) {
+      heap.dequeue();
+    }
+
+    const result = heap.front().value;
+    return result;
+  }
+
+  // You can use a simple heap that does binary search insert in a pinch.
+  function findKthLargestBinarySearch(nums: number[], k: number): number {
+    const heap = new BinarySearchMaxHeap<number>();
+    for (const num of nums) {
+      heap.enqueue(num);
+    }
+
+    for (let i = 0; i < k - 1; i++) {
+      heap.dequeue();
+    }
+
+    const result = heap.front().value;
+    return result;
+  }
   test('find kth largest - test case 1', async () => {
     expect(findKthLargest([3, 2, 1, 5, 6, 4], 2)).toBe(5);
+    expect(findKthLargestSimple([3, 2, 1, 5, 6, 4], 2)).toBe(5);
+    expect(findKthLargestBinarySearch([3, 2, 1, 5, 6, 4], 2)).toBe(5);
   });
 
   test('find kth largest - test case 2', async () => {
     expect(findKthLargest([3, 2, 3, 1, 2, 4, 5, 5, 6], 4)).toBe(4);
+    expect(findKthLargestSimple([3, 2, 3, 1, 2, 4, 5, 5, 6], 4)).toBe(4);
+    expect(findKthLargestBinarySearch([3, 2, 3, 1, 2, 4, 5, 5, 6], 4)).toBe(4);
   });
 
   test.skip('find kth largest - test case 3', async () => {
@@ -45,5 +80,7 @@ describe('kth largest element in an array', () => {
     const array = JSON.parse(data);
 
     expect(findKthLargest(array, 50000)).toBe(1);
+    expect(findKthLargestSimple(array, 50000)).toBe(1);
+    expect(findKthLargestBinarySearch(array, 50000)).toBe(1);
   });
 });
