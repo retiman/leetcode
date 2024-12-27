9822c57a27686630f9107e9251f285709de4ccc9
Min Huang <min.huang@alumni.usc.edu>
2024-12-26 22:22:15 -0800

    is same tree

--- test/tree/same-tree.test.ts
+++ test/tree/same-tree.test.ts
@@ -1,29 +1,7 @@
-// DIFFICULTY: Easy
-//
-// Given the roots of two binary trees p and q, write a function to check if they are the same or not.
-//
-// Two binary trees are considered the same if they are structurally identical, and the nodes have the same value.
-//
-// See https://leetcode.com/problems/same-tree/
-import { TreeNode } from '../../src/tree/same-tree';
+import { TreeNode } from '../../src/tree/common/tree-node';
+import { isSameTree } from '../../src/tree/same-tree';
 
 describe('same tree', () => {
-  function isSameTree(p: TreeNode | null, q: TreeNode | null): boolean {
-    if (p === null) {
-      return q === null;
-    }
-
-    if (q === null) {
-      return p === null;
-    }
-
-    if (p.val !== q.val) {
-      return false;
-    }
-
-    return isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
-  }
-
   test('same tree - test case 1', async () => {
     expect(isSameTree(null, null)).toBe(true);
   });
