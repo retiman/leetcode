7484ed2db15fbae9e9723f1b0fa84538d8140937
Min Huang <57320+retiman@users.noreply.github.com>
2024-12-26 22:29:20 -0800

    ADHOC: Leverage common code in tree problems (#156)

--- src/tree/construct-quad-tree.ts
+++ src/tree/construct-quad-tree.ts
@@ -1,31 +1,59 @@
-// This class definition comes from the problem itself and we cannot change it, or else our submission will not be
-// accepted.
-export class _Node {
-  val: boolean;
+// DIFFICULTY: Hard
+//
+// Given a n * n matrix grid of 0's and 1's only. We want to represent grid with a Quad-Tree.
+//
+// Return the root of the Quad-Tree representing grid.
+//
+// See https://leetcode.com/problems/construct-quad-tree/
+// See https://en.wikipedia.org/wiki/Quadtree
+import { _Node } from './common/quad-tree';
+export { construct };
 
-  isLeaf: boolean;
-
-  topLeft: _Node | null;
-
-  topRight: _Node | null;
-
-  bottomLeft: _Node | null;
-
-  bottomRight: _Node | null;
-
-  constructor(
-    val?: boolean,
-    isLeaf?: boolean,
-    topLeft?: _Node,
-    topRight?: _Node,
-    bottomLeft?: _Node,
-    bottomRight?: _Node
-  ) {
-    this.val = val === undefined ? false : val;
-    this.isLeaf = isLeaf === undefined ? false : isLeaf;
-    this.topLeft = topLeft === undefined ? null : topLeft;
-    this.topRight = topRight === undefined ? null : topRight;
-    this.bottomLeft = bottomLeft === undefined ? null : bottomLeft;
-    this.bottomRight = bottomRight === undefined ? null : bottomRight;
+// SOLUTION:
+//
+// To make a quad tree, recursively divide the matrix into quadrants until each quadrant is all 1's or all 0's.
+function construct(_grid: number[][]): _Node | null {
+  if (_grid.length === 0 || _grid[0].length === 0) {
+    return null;
   }
+
+  return constructInternal(_grid, 0, 0, _grid.length);
+}
+
+function constructInternal(grid: number[][], row: number, column: number, size: number): _Node {
+  const node = new _Node();
+
+  // If the grid segment is uniform, we can create a single quad tree node and set the value to all 1's or 0's.
+  if (isUniform(grid, row, column, size)) {
+    node.val = grid[row][column] === 1;
+    node.isLeaf = true;
+    return node;
+  }
+
+  // If the grid segment here isn't uniform, we'll need to create 4 quadrants and construct nodes out of all 4 of
+  // them.
+  //
+  // Setting the `node.val` of this node is irrelevant as it's not a leaf.  We can just use the default value.
+  const half = size / 2;
+  node.isLeaf = false;
+  node.topLeft = constructInternal(grid, row, column, half);
+  node.topRight = constructInternal(grid, row, column + half, half);
+  node.bottomLeft = constructInternal(grid, row + half, column, half);
+  node.bottomRight = constructInternal(grid, row + half, column + half, half);
+
+  return node;
+}
+
+// Checks if a quadrant of the grid starting at [row, column] is uniformly all 1's or all 0's.
+function isUniform(grid: number[][], row: number, column: number, size: number) {
+  const value = grid[row][column];
+  for (let i = row; i < row + size; i++) {
+    for (let j = column; j < column + size; j++) {
+      if (grid[i][j] !== value) {
+        return false;
+      }
+    }
+  }
+
+  return true;
 }
--- test/tree/range-sum-of-bst.test.ts
+++ test/tree/range-sum-of-bst.test.ts
@@ -1,34 +1,9 @@
-// DIFFICULTY: Easy
-//
-// Given the root node of a binary search tree and two integers low and high, return the sum of values of all nodes
-// with a value in the inclusive range [low, high].
-//
-// See https://leetcode.com/problems/range-sum-of-bst/
-import { TreeNode, convert } from '../../src/tree/range-sum-of-bst';
+import { deserialize } from '../../src/tree/common/tree-node';
+import { rangeSumBST } from '../../src/tree/range-sum-of-bst';
 
 describe('range sum of bst', () => {
-  function rangeSumBST(root: TreeNode | null, low: number, high: number): number {
-    let sum = 0;
-
-    function traverse(node: TreeNode | null) {
-      if (node === null) {
-        return;
-      }
-
-      if (node.val >= low && node.val <= high) {
-        sum += node.val;
-      }
-
-      traverse(node.left);
-      traverse(node.right);
-    }
-
-    traverse(root);
-    return sum;
-  }
-
   test('range sum of bst - test case 1', async () => {
-    const root = convert([10, 5, 15, 3, 7, null, 18]);
-    expect(rangeSumBST(root, 7, 15)).toBe(32);
+    const tree = deserialize([10, 5, 15, 3, 7, null, 18]);
+    expect(rangeSumBST(tree, 7, 15)).toBe(32);
   });
 });
