43fac6877bedcb0567654911e728ed704de50419
Min Huang <min.huang@alumni.usc.edu>
2024-12-26 21:47:08 -0800

    vert order

---
+++ src/tree/common/binary-tree-vertical-order-traversal.ts
@@ -0,0 +1,12 @@
+// This class definition comes from the problem itself and we cannot change it, or else our submission will not be
+// accepted.
+export class TreeNode {
+    val: number
+    left: TreeNode | null
+    right: TreeNode | null
+    constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
+        this.val = (val===undefined ? 0 : val)
+        this.left = (left===undefined ? null : left)
+        this.right = (right===undefined ? null : right)
+    }
+}
