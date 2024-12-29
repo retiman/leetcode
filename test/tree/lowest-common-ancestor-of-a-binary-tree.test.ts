7a9a80eeba3b2be04f0338ddfc4b622c1cfbf0b8
Min Huang <57320+retiman@users.noreply.github.com>
2024-12-28 22:28:52 -0800

    SOLN: Lowest Common Ancestor of a Binary Tree (#162)

---
+++ test/tree/lowest-common-ancestor-of-a-binary-tree.test.ts
@@ -0,0 +1,10 @@
+import { deserialize } from '../../src/tree/common/tree-node';
+import { lowestCommonAncestor } from '../../src/tree/lowest-common-ancestor-of-a-binary-tree';
+
+describe('lowest common ancestor of a binary tree i', () => {
+  test('lowest common ancestor - test case 1', async () => {
+    const tree = deserialize([3, 5, 1, 6, 2, 0, 8, null, null, 7, 4]);
+
+    expect(lowestCommonAncestor(tree, tree!.left, tree!.right)?.val).toStrictEqual(3);
+  });
+});
