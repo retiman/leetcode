361440b77e6c718da0e02d4956d47d943bc7e0c4
Min Huang <min.huang@alumni.usc.edu>
2025-01-05 20:58:37 -0800

    stack

--- test/stack/valid-parentheses.test.ts
+++ test/stack/valid-parentheses.test.ts
@@ -1,46 +1,6 @@
-// DIFFICULTY: EASY
-//
-// Given a string s containing just the characters '(', ')', '{', '}', '[' and ']', determine if the input string is valid.
-//
-// An input string is valid if:
-//
-// Open brackets must be closed by the same type of brackets.
-// Open brackets must be closed in the correct order.
-// Every close bracket has a corresponding open bracket of the same type.
-//
-// See https://leetcode.com/problems/valid-parentheses/
+import { isValid } from '../../src/stack/valid-parentheses';
+
 describe('valid parentheses', () => {
-  function isValid(text: string) {
-    const map = new Map<string, string>();
-    map.set('(', ')');
-    map.set('{', '}');
-    map.set('[', ']');
-
-    const stack = new Array<string>();
-    for (let i = 0; i < text.length; i++) {
-      const c = text[i];
-
-      if (c === '(' || c === '{' || c === '[') {
-        stack.push(c);
-        continue;
-      }
-
-      if (c === ')' || c === '}' || c === ']') {
-        if (stack.length === 0) {
-          return false;
-        }
-
-        const open = stack.pop();
-        const close = map.get(open || '');
-        if (close !== c) {
-          return false;
-        }
-      }
-    }
-
-    return stack.length === 0;
-  }
-
   test('valid parentheses - test case 1', async () => {
     expect(isValid('()')).toBe(true);
   });
