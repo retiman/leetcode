a26555228a7e27dca86e13476b94aa8aa80e4d5a
Min Huang <min.huang@alumni.usc.edu>
2025-01-06 12:35:58 -0800

    update

--- src/heap/kth-largest-element.ts
+++ src/heap/kth-largest-element.ts
@@ -7,7 +7,7 @@
 // Can you solve it without sorting?
 //
 // See {@link https://leetcode.com/problems/kth-largest-element-in-an-array/}
-const { MaxPriorityQueue } = require('@datastructures-js/priority-queue');
+const { MinPriorityQueue } = require('@datastructures-js/priority-queue');
 export { findKthLargest };
 
 // SOLUTION:
@@ -19,20 +19,26 @@ export { findKthLargest };
 //
 // COMPLEXITY:
 //
-// The heap can be created in O(n * log(n)) time.  Removing k elements from the heap is O(k * log(n)), so the total
-// complexity is still O(n * log(n)).
+// The heap can be created in O(n * log(k)) time.
 function findKthLargest(nums: number[], k: number): number {
-  const heap = new MaxPriorityQueue();
+  // Use a min heap because we'll want to pop off the smallest element every time we reach the limit.
+  const heap = new MinPriorityQueue();
+
+  // Iterate through the numbers, and jam numbers on the heap until we have k elements.
   for (const num of nums) {
     heap.enqueue(num);
+
+    // Now we have the k + 1 smallest elements we've seen so far.  If we get a new element, pop from the
+    // heap, which will leave us with larger elements in the heap.
+    //
+    // At the end, we'll pop off all the smallest elements, leaving us with the top k largest elements.
+    if (heap.size() > k) {
+      heap.dequeue();
+    }
   }
 
-  // Now just remove k-1 elements and the top element of the heap will be the answer.
-  for (let i = 0; i < k - 1; i++) {
-    heap.dequeue();
-  }
-
-  // The kth largest element will be at the top of the queue.
+  // This is a min heap, so the smallest element is at the front.  However, there are k elements and we
+  // the kth smallest element, so it's at the top!
   const result = heap.front().element;
   return result;
 }
