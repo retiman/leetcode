82f63a9b0e64041108bc01f433ba282fd2d1191f
Min Huang <min.huang@alumni.usc.edu>
2025-01-13 17:53:16 -0800

    docs

--- src/heap/kth-largest-element.ts
+++ src/heap/kth-largest-element.ts
@@ -12,14 +12,30 @@ export { findKthLargest };
 
 // SOLUTION:
 //
-// You can use quick select to do the most efficient algorithm, but it's very complicated.  Instead, we'll just use
-// a heap to do this almost as efficiently.  This does not sort the array, but it is a bit slower than quick select.
+// You can do this with quick select, similar to quick sort.  The idea is to eliminate half of the search space at each
+// step.  You pick a pivot element (using heuristics, or even randomly), then partition the array into two halves: left
+// half has elements smaller than the pivot, right half has elements larger than the pivot.
+//
+// After the partition, you can check if the pivot is the (n - k)th element.  If it is, you're done.  If not, recurse
+// into the left or right partitions depending on the pivot's position.
 //
 // That said, the leetcode questions seem to be crafted to ruin poor pivot index choices.
 //
+// The other way to do this is to use a heap.  You can do this an efficient way or an inefficient way.  The inefficient
+// way involves jamming all the elements onto a max heap, then popping off k elements to get the kth largest element.
+//
+// The efficient way is to maintain a min heap and only ever allow k elements on the heap.  This way, each time you pop
+// an element off the heap, you're left with ever larger elements.  Since the heap size is k, the kth largest element
+// will be at the top of the heap.
+//
 // COMPLEXITY:
 //
-// The heap can be created in O(n * log(k)) time.
+// The quick select method has an average time complexity of O(n), but a worst case time complexity of O(n^2),
+// especially if you pick the wrong pivot each time.  However, the space complexity is O(1).
+//
+// The min heap approach is O(n log k) time complexity, and O(k) space complexity.
+//
+// The latter offers you more consistent run times.
 function findKthLargest(nums: number[], k: number): number {
   // Use a min heap because we'll want to pop off the smallest element every time we reach the limit.
   const heap = new MinPriorityQueue();
