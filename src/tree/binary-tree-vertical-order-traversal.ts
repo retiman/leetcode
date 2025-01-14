430c9336ee4e54bf732a51237d81242c817dddc0
Min Huang <min.huang@alumni.usc.edu>
2025-01-13 19:28:22 -0800

    docs

--- src/tree/binary-tree-vertical-order-traversal.ts
+++ src/tree/binary-tree-vertical-order-traversal.ts
@@ -20,68 +20,67 @@ export { verticalOrder };
 //
 // COMPLEXITY:
 //
-// The BFS will run in O(n) time, and the sorting will run in O(n * log(n)) time.  We could, in theory, have a very
-// imbalanced tree where the sorting dominates the time complexity.
+// The BFS will run in O(n) time where n is the number of nodes.  The result computation at the end will run in O(k)
+// time where k is the number of columns.  However, you're always guaranteed to have more nodes than columns (or the
+// same) so overall time complexity is still O(n).
+//
+// Space complexity is O(n) because we are storing k columns mapping to lists of nodes, but the overall number of nodes
+// stored in the map is at most n.  We are also using a queue, which has at most n nodes in it.
 function verticalOrder(root: TreeNode | null): number[][] {
-  type Row = number;
   type Column = number;
-  type ExtendedNode = {
-    node: TreeNode;
-    row: Row;
-    column: Column;
-  };
 
   if (root === null) {
     return [];
   }
 
-  // Keep a map of column to nodes with their row values, so we can order by row later.
-  const map = new Map<Column, ExtendedNode[]>();
-  const queue: ExtendedNode[] = [{ node: root, row: 0, column: 0 }];
+  // Keep a map of column to nodes.  Note that we don't actually need to store their row values, because if we visit
+  // the nodes in the right order, the array will already be sorted by row.  The key is to push the left child before
+  // the right child.
+  const map = new Map<Column, TreeNode[]>();
 
-  // Keep track of the min and max columns so we can iterate over them later.
+  // We DO need to store the column values in our BFS queue though.  That's because as we shift items off the queue, we
+  // will need to know which column it's in, so we can add it to the TreeNode array in the map above.
+  type ExtendedNode = [TreeNode, Column];
+  const queue: ExtendedNode[] = [[root, 0]];
+
+  // Finally, we have to keep track of the min and max columns so we know where to start with the vertical order list.
   let min = 0;
   let max = 0;
 
+  // The rest of the algorithm is a standard BFS.  We don't need to maintain a visited set because the nodes are
+  // guaranteed to be organized as a tree, so we can't visit the same node twice.
   while (queue.length > 0) {
-    const u = queue.shift()!;
+    const [node, column] = queue.shift()!;
 
     // Add the node to this map for bookkeeping.
-    if (!map.has(u.column)) {
-      map.set(u.column, []);
+    if (!map.has(column)) {
+      map.set(column, []);
     }
-    map.get(u.column)!.push(u);
+    map.get(column)!.push(node);
 
     // Update min/max columns.
-    min = Math.min(min, u.column);
-    max = Math.max(max, u.column);
+    min = Math.min(min, column);
+    max = Math.max(max, column);
 
-    // Enqueue the children as usual in BFS.
-    if (u.node.left !== null) {
-      queue.push({
-        node: u.node.left,
-        row: u.row + 1,
-        column: u.column - 1
-      });
+    // Enqueue the children as usual in BFS, but make sure to enqueue the LEFT child first to keep our proper row order.
+    if (node.left !== null) {
+      queue.push([node.left, column - 1]);
     }
 
-    if (u.node.right !== null) {
-      queue.push({
-        node: u.node.right,
-        row: u.row + 1,
-        column: u.column + 1
-      });
+    // Enqueue the children as usual in BFS, but make sure to enqueue the RIGHT child last.
+    if (node.right !== null) {
+      queue.push([node.right, column + 1]);
     }
   }
 
+  // Now we have a map of Column -> TreeNode[], and for each column, we just print out the values in the row.
   const result: number[][] = [];
   for (let column = min; column <= max; column++) {
-    // Sort the nodes by the row index to get the vertical order for this particular column.
-    const nodes = map.get(column)!;
-    nodes.sort((a, b) => a.row - b.row);
+    // These nodes are already in row order because we visited the left child before the right.
+    const rows = map.get(column)!;
 
     // Now map each extended node to its value for the final result.
-    const values = nodes.map(u => u.node.val);
+    const values = rows.map(node => node.val);
     result.push(values);
   }
 
