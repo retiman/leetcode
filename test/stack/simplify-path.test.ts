a045dcd2b6512f398754d6078f13517aa3a8570b
Min Huang <min.huang@alumni.usc.edu>
2025-01-14 22:32:16 -0800

    docs

--- test/stack/simplify-path.test.ts
+++ test/stack/simplify-path.test.ts
@@ -1,7 +1,11 @@
 import { simplifyPath } from '../../src/stack/simplify-path';
 
 describe('simplify path', () => {
-  test('simplify path', () => {
+  test('simplify path - test case 1', () => {
     expect(simplifyPath('/home/')).toEqual('/home');
+  });
+
+  test('simplify path - test case 2', () => {
+    expect(simplifyPath('/../')).toEqual('/home');
   });
 });
