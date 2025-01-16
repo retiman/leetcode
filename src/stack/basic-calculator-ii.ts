49ce8354f6498cdd05d0410e2f7416d12d66595d
Min Huang <min.huang@alumni.usc.edu>
2025-01-15 19:25:26 -0800

    calc2

--- src/stack/basic-calculator-ii.ts
+++ src/stack/basic-calculator-ii.ts
@@ -18,90 +18,112 @@ export { calculate };
 // Assumption is that we only have numbers, +, -, *, and / in the expression.  We also assume no parentheses for
 // grouping expressions.
 //
-// The * and / operators have higher precedence and are evaluated immediately in the order we see them.
+// To do this we iterate through the string and use a stack to store intermediate results of computation.  Because the
+// '*' and '/' operators have higher precedence, we'll resolve them immediately.  Those intermediate results will be
+// stored onto the stack and we'll resolve the '+' and '-' operators at the end.
 //
-// Note that the - operator can be a unary operator.
+// Take note that the '-' operator can be a unary operator.
+//
+// Here's an example evaluation.  Let's suppose we have '33+2*2'.  Whenever we resolve an operator, OR start the
+// calculator, we'll assume the first number is 0 (no value) and the last operator is '+'.
+//
+// - When we see c = '3', we'll store it as the current value as '3'.
+// - When we see c = '3' again, we'll set the current value as '3 *10 + 3' or '33'.
+// - When we see c = '+', we'll resolve the current value '33' on the stack.  In this step, the current operator c is
+//   '+' and the last operator is also '+'.  Based on the last operator being '+', we push onto the stack.  Now we set
+//   the last operator to '+'.
+// - When we see c = '2', we'll store it as the current value as '2'.
+// - When we see c = '*', we'll resolve the current value '2'.  In this step, the current operator 'c' is '*' and the
+//   last operator is '+'.  Based on the last operator being '+', we push onto the stack.  Now we set the last operator
+//   to '*'.
+// - When we see c = '2', we'll store it as the current value as '2'.
+// - Because it is the last character, we resolve the last operator, which is '*'.  So we pop from the stack ('2'), and
+//   multiply by '2', giving a result '4', which we push to the stack.
+// - Finally we sum the stack.
+//
+// In a situation where the last operator is '-', we would push a negative value onto the stack.  For example, if we
+// have '3-2', when we see '2', the last operator is '-', so we push '-2' onto the stack.
+//
+// Similarly, if we just have '-2', we will see the '2' and push '-2' onto the stack, since the last operator is '-'.
 function calculate(s: string): number {
-  function isOperator(c: string) {
-    return c === '+' || c === '-' || c === '*' || c === '/';
+  function isDigit(c: string) {
+    return c >= '0' && c <= '9';
   }
 
   // Keep a stack of numbers to add up; the multiply and divide operations will be applied immediately since they have
   // higher precedence.
   const stack: number[] = [];
 
-  // Save the last seen operator and any intermediate text that is part of a number.  Once we build up a complete number
-  // we'll resolve the operation.
+  // Keep track of the current value as we iterate through the string.  Every time we see a digit, we can multiply the
+  // number by 10 and add the digit to it.
+  let value = 0;
+
+  // Note that the action we take when we see an operator depends on the LAST operator we saw, not the current operator
+  // we see.
   //
-  // Set the initial operator to '+' since the default operation is to add.
-  let op = '+';
-  let text = '';
+  // Before we've seen any operators, the default behavior is to add (not subtract or something), so set the last
+  // operator to '+'.
+  let last = '+';
 
   for (let i = 0; i < s.length; i++) {
     const c = s[i];
 
-    // If we find a number, we should and it to the `text` value to continue building up the number.
-    //
-    // Do not continue to the next character just yet, since we may be at the end of the string and will need to resolve
-    // this number immediately.
-    const isDigit = !isOperator(c) && c !== ' ';
-    if (isDigit) {
-      text += c;
+    // If we see a space, we can safely skip it.  UNLESS, it is the last character in a string.  In that case, we do
+    // actually need to resolve the last operation we saw.  For example, '3 * 2 ' should resolve '3 * 2'.  Here, we'll
+    // just skip spaces if they are not the last character.
+    if (c === ' ' && i !== s.length - 1) {
+      continue;
     }
 
-    // If it's not a digit, then it's a space or an operator.  If it's an operator, we'll want to resolve the current
-    // number and operator.
-    //
-    // If it's a space, we'll generally just ignore it.
-    //
-    // However, regardless of what the character is (even if it's a space), if we are at the end of the string, we'll
-    // have to resolve the current number and last operator.
-    if (isOperator(c) || i === s.length - 1) {
-      // Convert the number string we've built up to an actual number.
-      const num = Number.parseInt(text, 10 /* radix */);
+    // If we find a digit, build up the current number by multiplying the current value by 10 and adding the digit.
+    if (isDigit(c)) {
+      value = value * 10 + Number.parseInt(c, 10 /* radix */);
 
-      // At this point we've seen an operator, 'c'.  The operator 'c' will not tell us what to do NOW, but what to do in
-      // the future, after we've resolved the current operator.
-      //
-      // There is a bit of a gotcha here; if the last character is a ' ', there is no "next operator", that is, if we
-      // have "3/2 ", the next operator ends up being ' '.  That said, it doesn't affect the calculation since the
-      // iteration ends anyways.
-      const nextOperator = c;
-
-      // If the last operator was '+', we'll resolve them all at the end, so just push it onto the stack.
-      if (op === '+') {
-        stack.push(num);
+      // Normally, we could just continue, but if the digit is the very last character, like '3 * 2', we do actually
+      // need to fall through to the operation resolution section below.
+      if (i !== s.length - 1) {
+        continue;
       }
-      // If the last operator was '-' (even if it was unary minus), we'll push the negative value onto the stack and
-      // resolve the additions at the end.
-      else if (op === '-') {
-        stack.push(-num);
-      }
-      // If the last operator was '*' or '/', we'll want to resolve it immediately, since these operations take
-      // precedence.  The last number on the stack will be the left operand and the current number will be the right
-      // operand.
-      //
-      // After resolving the '*' operation, push the result onto the stack and resolve the additions at the end.  If we
-      // had a '-' operator, this will result in a negative value pushed onto the stack, which is what we want.
-      else if (op === '*') {
-        const left = stack.pop()!;
-        const right = num;
-        const value = left * right;
-        stack.push(value);
-      }
-      // If the last operator was '/', just keep in mind that the stack value is the left operand and the current number
-      // is the right operand.  The instructions say to truncate towards zero.
-      else if (op === '/') {
-        const left = stack.pop()!;
-        const right = num;
-        const value = Math.trunc(left / right);
-        stack.push(value);
-      }
-
-      // After resolving an operation, reset the text value, and set the next operator.
-      op = nextOperator;
-      text = '';
     }
+
+    // Now we have a non-digit character.  If it's a space, we can ignore it, but ONLY if it's not the last character.
+    // If it's the last character, we'll have to resolve the current number and operator.
+    // Okay, now we have either a space or a non-digit character that could potentially be an operation.  Either way
+    // we need to resolve an expression.  For example, reading up to the last character in both strings results in some
+    // evaluation:
+    //
+    // - '3 * 2 +' -> Results in resolving '3 * 2' first, then adding the result to the stack.
+    // - '3 * 2  ' -> Results in resolving '3 * 2' because we are at the last value, but it's a string and not an
+    //                operator!
+    //
+    // Remember, the current operator tells us what to do LATER.  What we do now depends on the last operator.  If the
+    // last operator was '+', we delay the summation until the end.
+    if (last === '+') {
+      stack.push(value);
+    }
+    // Similarly if the operator was '-', we delay the summation until the end.
+    else if (last === '-') {
+      stack.push(-value);
+    }
+    // If the operator was '*', we do want to resolve it immediately with the current value and the value on the stack.
+    // Imagine we've hit a case like '2 * 2 +' and we've just read the '+'.  We'll want to resolve '2 * 2' first.
+    else if (last === '*') {
+      const left = stack.pop()!;
+      const right = value;
+      stack.push(left * right);
+    }
+    // If the last operator was '/', we similarly want to resolve it immediately.  Take care to do left / right where
+    // the left value is on the stack and the right value is what number we just read.
+    else if (last === '/') {
+      const left = stack.pop()!;
+      const right = value;
+      stack.push(Math.trunc(left / right));
+    }
+
+    // Now, after resolving an operation, reset the current value and store the current operator as the last one we have
+    // seen.
+    last = c;
+    value = 0;
   }
 
   const result = stack.reduce((a, b) => a + b, 0);
