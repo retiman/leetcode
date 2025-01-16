7b4518ca2c7d90197eac4d9b438023314630cdaa
Min Huang <min.huang@alumni.usc.edu>
2025-01-15 20:22:46 -0800

    docs

--- src/tree/lowest-common-ancestor-of-a-binary-tree.ts
+++ src/tree/lowest-common-ancestor-of-a-binary-tree.ts
@@ -14,41 +14,67 @@ export { lowestCommonAncestor };
 // In this problem, we are not given the parent nodes.  So we have to solve this problem using a recursive divide and
 // conquer approach.
 //
-// The idea is that if p and q are in different subtrees (one in the left, one in the left), then the current node is
-// their lowest common ancestor.
+// The idea is to recursively check the left and right subtrees for nodes p and q.  There are three cases:
 //
-// However, if p and q are in the same subtree, then we can't make any determination about their LCA, so we have to
-// continue the search down that subtree.
+// 1. We find p and q in DIFFERENT subtrees.
+// 2. We find p and q BOTH in the left subtree.
+// 3. We find p and q BOTH in the right subtree.
+//
+// Well, in case 1, if we began this process from the root, that's it!  The current node has to be the LCA.  If instead
+// we discover that p and q are BOTH in the left or right subtree, make that subtree the new root and continue the
+// search.
+//
+// Note that this solution requires that both p and q exist in the tree.  If one of them doesn't exist we could return
+// a false LCA.  To guard against this we'd have to check the tree beforehand to ensure that both p and q exist.
 //
 // COMPLEXITY:
 //
 // The recursive calls visit every node once, so the time complexity is O(n) in number of nodes.  We don't use extra
 // space but the call stack uses O(h) space where h is the height of the tree.
-function lowestCommonAncestor(root: TreeNode | null, p: TreeNode | null, q: TreeNode | null): TreeNode | null {
-  // We couldn't find p or q in either of the subtrees.
-  if (root === null) {
-    return null;
+function lowestCommonAncestor(node: TreeNode | null, p: TreeNode | null, q: TreeNode | null): TreeNode | null {
+  // Recursively find p or q in the tree rooted at node.
+  function findNode(node: TreeNode | null, p: TreeNode | null, q: TreeNode | null): TreeNode | null {
+    // We DID NOT find p or q in either of the subtrees.
+    if (node === null) {
+      return null;
+    }
+
+    // We DID find p or q in either of the subtrees, so return it.
+    if (node === p || node === q) {
+      return node;
+    }
+
+    // Otherwise, continue by checking for p and q in the left and right subtrees.
+    //
+    // If left is non-null, then we found one of p or q in the left subtree.
+    // If right is non-null, then we found one of p or q in the right subtree.
+    // If both were non-null, then we found p and q in different subtrees.
+    const left = findNode(node.left, p, q);
+    const right = findNode(node.right, p, q);
+
+    // If BOTH left and right are non-null, then we found p and q in different subtrees.  If that is the case, the
+    // current node must be the LCA.
+    if (left !== null && right !== null) {
+      return node;
+    }
+
+    // We found ONE of nodes p or q in the LEFT subtree.  However, we didn't find the other node in the right subtree
+    // so we assume that they were both in the left subtree.  Note that we ASSUME that both p and q exist in the tree,
+    // or else we could return a false LCA.
+    if (left !== null) {
+      return left;
+    }
+
+    // We found ONE of nodes p or q in the RIGHT subtree.  However, we didn't find the other node in the left subtree
+    // so we assume that they were both in the right subtree.  Note that we ASSUME that both p and q exist in the tree,
+    // or else we could return a false LCA.
+    return right;
   }
 
-  // We a found in one of the left or right subtrees, so return it.
-  if (root === p || root === q) {
-    return root;
-  }
-
-  // Check for p and q in the left and right subtrees.
-  const left = lowestCommonAncestor(root.left, p, q);
-  const right = lowestCommonAncestor(root.right, p, q);
-
-  // We found BOTH nodes in DIFFERENT subtrees, so the current node is the LCA.
-  if (left !== null && right !== null) {
-    return root;
-  }
-
-  // We found BOTH nodes in the LEFT subtree, so some node in the left subtree is the LCA.
-  if (left !== null) {
-    return left;
-  }
-
-  // We found BOTH nodes in the RIGHT subtree, so some node in the right subtree is the LCA.
-  return right;
+  // Technically, lowestCommonAncestor has the same signature as findNode, so we could've just implemented the LCA
+  // logic directly.
+  //
+  // However, lowestCommonAncestor will return to you the LCA always.  In findNode, sometimes the intermediate result
+  // is not the LCA (e.g. when we are just looking for the node).
+  return findNode(node, p, q);
 }
