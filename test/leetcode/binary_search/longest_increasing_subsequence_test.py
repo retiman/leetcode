254f31dd96d372e3aaa88ada985f05eafdab0e83
Min Huang <min.huang@alumni.usc.edu>
2025-03-10 14:46:56 -0700

    lis

---
+++ test/leetcode/binary_search/longest_increasing_subsequence_test.py
@@ -0,0 +1,8 @@
+from leetcode.binary_search.longest_increasing_subsequence import Solution
+
+
+soln = Solution()
+
+
+def test_case_1():
+    assert soln.lengthOfLIS([10, 9, 2, 5, 3, 7, 101, 18]) == 4
