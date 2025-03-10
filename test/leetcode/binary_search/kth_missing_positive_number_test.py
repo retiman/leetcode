996b5fac0be31cc8fa21655060005d40ebb6da08
Min Huang <min.huang@alumni.usc.edu>
2025-03-10 14:23:25 -0700

    kth missing positive number

---
+++ test/leetcode/binary_search/kth_missing_positive_number_test.py
@@ -0,0 +1,12 @@
+from leetcode.binary_search.kth_missing_positive_number import Solution
+
+
+soln = Solution()
+
+
+def test_case_1():
+    assert soln.findKthPositive([2, 3, 4, 7, 11], 5) == 9
+
+
+def test_case_2():
+    assert soln.findKthPositive([1, 2, 3, 4], 2) == 6
