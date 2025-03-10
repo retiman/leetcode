8e3dab4446a2b053539f79567ebdb8f6f25daff5
Min Huang <min.huang@alumni.usc.edu>
2025-03-09 23:44:10 -0700

    fix type issues

--- src/leetcode/binary_search/README.md
+++ src/leetcode/binary_search/README.md
@@ -12,18 +12,19 @@ Use this to check if an element exists in the array.  The `left` and `right` poi
 Note that this can be used like left most duplicate to find a good insertion point as well.
 
 ```
-let left = 0, right = xs.length - 1;
-while (left <= right) {
-  const m = Math.floor((left + right) / 2);
-  if (xs[m] < target) {
-    left = m + 1;
-  } else if (xs[m] > target) {
-    right = m - 1;
-  } else {
-    return m;
-  }
-}
-return -1;
+left = 0
+right = len(xs) - 1
+
+while left <= right:
+    m = (left + right) // 2
+    if xs[m] < target:
+        left = m + 1
+    elif xs[m] > target:
+        right = m - 1
+    else:
+        return m
+
+return -1
 ```
 
 ### Left-most Duplicate
@@ -31,24 +32,25 @@ return -1;
 Use this to check where an element can be inserted into the array.  The `left` and `right` pointers are not always within bounds (because the element could be inserted at the end of the array).  Exiting the while loop indicates the element was found or you've found a good place to insert.
 
 ```
-let left = 0, right = xs.length;
-while (left < right) {
-  const m = Math.floor((left + right) / 2);
-  if (xs[m] < target) {
-    left = m + 1;
-  } else {
-    right = m;
-  }
-}
-return left < xs.length && xs[left] === target ? left : -1;
+left = 0
+right = len(xs)
+
+while left < right:
+    m = (left + right) // 2
+    if xs[m] < target:
+        left = m + 1
+    else:
+        right = m
+
+return left < len(xs) && xs[left] === target and left or -1
 ```
 
 ## Common Phrases
 
 These phrases indicate binary search might be useful:
 
-- 'sorted array'
-- 'find the position or index'
+- sorted array
+- find the position or index
 
 ## Boundaries
 
---
+++ src/leetcode/heap/k_closest_points_to_origin.py
@@ -0,0 +1,59 @@
+# DIFFICULTY: MEDIUM
+#
+# Given an array of points where points[i] = [xi, yi] represents a point on the X-Y plane and an integer k, return the
+# k closest points to the origin (0, 0).
+#
+# The distance between two points on the X-Y plane is the Euclidean distance (i.e., √(x1 - x2)2 + (y1 - y2)2).
+#
+# You may return the answer in any order. The answer is guaranteed to be unique (except for the order that it is in).
+#
+# See https://leetcode.com/problems/k-closest-points-to-origin
+from heapq import heappush, heappop
+from math import sqrt
+
+
+class Solution:
+    def kClosest(self, points: list[list[int]], k: int) -> list[list[int]]:
+        """
+        SOLUTION:
+
+        The request for "k-closest" indicates you'll want a heap.  Using a either a min heap or max heap will work.
+
+        With a min heap, just jam all the points on the heap and then slice the first k elements.  This requires storing all
+        the points on the heap.
+
+        With a max heap, you can jam points on the heap until you have k elements.  Then, when you have a new point, compare
+        it to the max element on the heap.  If it's closer, pop off the max element and jam it on the heap; otherwise, ignore
+        it.
+
+        The max heap approach can be more efficient, so we'll go that route.
+
+        COMPLEXITY:
+
+        With a min heap, it will be O(n * log(n)) to insert all the points on the heap.
+
+        With a max heap, if k is much smaller than n, it will be O(n * log(k)) to insert all the points on the heap.
+        """
+
+        # Calculates the distance to the origin.  Technically, since we're only using the distance to do comparison, we
+        # don't need to take the square root since they are all going to compare the same square root or not.  I'm
+        # leaving it here for correctness.
+        def distance(p: list[int]) -> float:
+            [x, y] = p
+            return sqrt(x**2 + y**2)
+
+        # Store max heap of (distance, point).  LeetCode gives us the points as list of [x, y].
+        max_heap: list[tuple[float, list[int]]] = []
+        for p in points:
+            # Simulate a max heap by negating the distance.  When storing a tuple in the heap, Python will sort by tuple
+            # ordering, so will first sort by the first element, then the second.  Sorting by the first element
+            # (distance) is all we need.
+            d = distance(p)
+            heappush(max_heap, (-d, p))
+
+            # If we have more than k elements, remove the farthest point.
+            if len(max_heap) > k:
+                heappop(max_heap)
+
+        # Return the k closest points, throwing away the distance.
+        return [p for _, p in max_heap]
