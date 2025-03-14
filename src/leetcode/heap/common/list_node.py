c48b19afc2b82d9923c8d22cbe4538dec77fe14c
Min Huang <min.huang@alumni.usc.edu>
2025-03-13 21:03:22 -0700

    comments

--- src/leetcode/heap/common/list_node.py
+++ src/leetcode/heap/common/list_node.py
@@ -1,3 +1,5 @@
+# This class definition comes from the problem itself and we cannot change it, or else our submission will not be
+# accepted.
 class ListNode:
     def __init__(self, val=0, next=None):
         self.val = val
