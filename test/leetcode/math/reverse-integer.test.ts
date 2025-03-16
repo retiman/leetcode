5c8b9e5688e9c518400e34403349ecd073c4271a
Min Huang <min.huang@alumni.usc.edu>
2025-03-16 15:00:00 -0700

    reverse integer

--- test/leetcode/math/reverse-integer.test.ts
+++
@@ -1,7 +0,0 @@
-import { reverse } from '../../src/math/reverse-integer';
-
-describe('reverse integer', () => {
-  test('reverse integer - test case 1', async () => {
-    expect(reverse(123)).toBe(321);
-  });
-});
