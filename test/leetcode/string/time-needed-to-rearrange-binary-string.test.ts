4be13ca299f8f1eb5718ea391c9f763bcb8f57ee
Min Huang <min.huang@alumni.usc.edu>
2025-03-19 16:41:56 -0700

    time to rearrange

--- test/leetcode/string/time-needed-to-rearrange-binary-string.test.ts
+++
@@ -1,7 +0,0 @@
-import { secondsToRemoveOccurrences } from '../../src/string/time-needed-to-rearrange-binary-string';
-
-describe('time needed to rearrange a binary string', () => {
-  test('time needed to rearrange a binary string - test case 1', async () => {
-    expect(secondsToRemoveOccurrences('0110101')).toBe(4);
-  });
-});
---
+++ test/leetcode/string/time_needed_to_rearrange_binary_string_test.py
@@ -0,0 +1,8 @@
+from leetcode.string.time_needed_to_rearrange_binary_string import Solution
+
+
+soln = Solution()
+
+
+def test_case_1():
+    assert soln.secondsToRemoveOccurrences("0110101") == 4
