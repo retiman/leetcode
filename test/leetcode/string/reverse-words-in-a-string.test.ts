09a1300245b9fb4217dc0c9069ee7c070bf5b544
Min Huang <min.huang@alumni.usc.edu>
2025-03-19 16:31:24 -0700

    reverse words

--- test/leetcode/string/reverse-words-in-a-string.test.ts
+++
@@ -1,7 +0,0 @@
-import { reverseWords } from '../../src/string/reverse-words-in-a-string';
-
-describe('reverse words in a string', () => {
-  test('reverse words in a string - test case 1', async () => {
-    expect(reverseWords('the sky is blue')).toBe('blue is sky the');
-  });
-});
---
+++ test/leetcode/string/reverse_words_in_a_string_test.py
@@ -0,0 +1,8 @@
+from leetcode.string.reverse_words_in_a_string import Solution
+
+
+soln = Solution()
+
+
+def test_case_1():
+    assert soln.reverseWords("the sky is blue") == "blue is sky the"
